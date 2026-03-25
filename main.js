7344e26d1626e66cc4e2f3b1a5048315c7194826
MrD-304 <greatdragon4614@gmail.com>
2026-03-25 12:03:22 +0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function joinWithCharacter(array, charactor) {
-  var array = ["Honda", "Mazda", "Mercedes"];
-  var charactor = [1, 2, 3];
-  var result = array.concat(charactor);
-  console.log(result.join(" _ "));
-  return result;
-    console.log(1);
-}
